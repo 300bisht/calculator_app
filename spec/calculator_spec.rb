8bd38fddbcfe0445b9a206749bbbf766dcac145a
sandeep <300bisht@gmail.com>
2024-03-17 16:07:14 +0530

    checking sum of the two numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("")).to eql(0)
       end
     end
+
+    context "given two numbers separated by comma" do
+      it "returns the sum of the two numbers" do
+        expect(StringCalculator.add("3,4")).to eql(7)
+      end
+    end
   end
 end
