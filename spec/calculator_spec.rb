bd223606bc0e4ad75c83f3370702bea58110f562
sandeep <300bisht@gmail.com>
2024-03-17 16:13:42 +0530

    given numbers with newlines between them

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -19,5 +19,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1,2,3,4,5")).to eql(15)
       end
     end
+
+    context "given numbers with newlines between them" do
+      it "returns the sum of all numbers" do
+        expect(StringCalculator.add("1\n2,3\n4,5")).to eql(15)
+      end
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,14 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
-    digits = input.split(',').map{|x| x.to_i}
-    digits.reduce(0, :+)
+    digits(input).reduce(0, :+)
+  end
+
+  def self.digits(input)
+  	input.gsub("\n", delimiter).split(delimiter).map{|x| x.to_i}
+  end
+
+  def self.delimiter
+  	','
   end
 end
